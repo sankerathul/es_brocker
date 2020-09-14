ec2d095702b9ecbfacc58e2dce162e628b55020c
athul <athul@wandx.co>
2020-09-14 17:17:31 +0530

    ui update

--- routes.py
+++ routes.py
@@ -13,28 +13,28 @@ CORS(app,resources={r"/*": {"origins": "*.*"}})
 def hello_world():
     return render_template("index.html")
 
-# @app.route('/get_data', methods=['POST'])
-# def get_es_data():
-#     data = request.json
-#     try:
-#         higher_limit = datetime.strptime(data["higher_limit"], '%Y-%m-%d %H:%M:%S.%f')
-#         lower_limit = datetime.strptime(data["lower_limit"], '%Y-%m-%d %H:%M:%S.%f')
+@app.route('/get_data', methods=['POST'])
+def get_es_data():
+    data = request.json
+    try:
+        higher_limit = datetime.strptime(data["higher_limit"], '%Y-%m-%d %H:%M:%S.%f')
+        lower_limit = datetime.strptime(data["lower_limit"], '%Y-%m-%d %H:%M:%S.%f')
 
-#         res,file_name = get_bucket_aggregate(higher_limit, lower_limit)
-#     except:
-#         res,file_name = get_bucket_aggregate()
+        res,file_name = get_bucket_aggregate(higher_limit, lower_limit)
+    except:
+        res,file_name = get_bucket_aggregate()
     
-#     print(file_name)
-#     download_link = "http://52.56.164.147:3009/get_csv/{}".format(file_name)
-#     return jsonify({"url":download_link})
-#     # return res
+    print(file_name)
+    download_link = "http://52.56.164.147:3009/get_csv/{}".format(file_name)
+    return jsonify({"url":download_link})
+    # return res
 
-# @app.route("/get_csv/<file_name>")
-# def get_csv(file_name):
-#     try:
-#         return send_file(file_name, as_attachment=True, cache_timeout=0)
-#     except FileNotFoundError:
-#         abort(404)
+@app.route("/get_csv/<file_name>")
+def get_csv(file_name):
+    try:
+        return send_file(file_name, as_attachment=True, cache_timeout=0)
+    except FileNotFoundError:
+        abort(404)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
