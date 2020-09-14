b8533eb580b5f20f200d4b8e7565813bc79eccfd
athul <athul@wandx.co>
2020-09-14 17:33:53 +0530

    ui update

--- routes.py
+++ routes.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 import time
 
 app = Flask(__name__)
-CORS(app,resources={r"/*": {"origins": "*.*"}})
+CORS(app,resources={{"origins": "*.*"}})
 
 @app.route('/')
 def hello_world():
